da2498ad3c62716a3c153505d15429ee4133db29
lily <lngo@cloudflare.com>
2025-07-09 20:47:11 -0500

    update message

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+
+const app = new Hono<{ Bindings: Env }>();
+
+const DESIRED_HOST = "hello.interns.workers.dev";
+
+app.use("*", async (c, next) => {
+	const currentHost = c.req.header("host");
+
+	if (currentHost !== DESIRED_HOST) {
+		const url = new URL(c.req.url);
+		url.host = DESIRED_HOST;
+
+		return c.redirect(url.toString());
+	}
+
+	await next();
+});
+
+const message = `Hello, Austin!
+
+- lily, Isaac, and Aaron
+Cloudflare Summer 2025 Interns`;
+
+app.get("/", (c) => {
+	return c.text(message);
+});
 
 export default {
-	async fetch(_request, _env, _ctx): Promise<Response> {
-		return new Response("Hello World!");
-	},
+	fetch: app.fetch,
 } satisfies ExportedHandler<Env>;
